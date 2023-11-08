f1d7205c8f26332454a5e9361c237be2f13ce6ab
bartooo <bcywinski11@gmail.com>
2023-11-08 21:15:52 +0100

    Add fields validation

--- Python/Flask_Book_Library/project/books/models.py
+++ Python/Flask_Book_Library/project/books/models.py
@@ -1,5 +1,6 @@
 from project import db, app
 import re
+from sqlalchemy.orm import validates
 
 
 # Book model
@@ -8,7 +9,7 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, index=True)
     author = db.Column(db.String(64))
-    year_published = db.Column(db.Integer) 
+    year_published = db.Column(db.Integer)
     book_type = db.Column(db.String(20))
     status = db.Column(db.String(20), default='available')
 
@@ -18,6 +19,22 @@ class Book(db.Model):
         self.year_published = year_published
         self.book_type = book_type
         self.status = status
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name.isalnum():
+            raise ValueError('Name must contain only alphanumeric characters')
+        if len(name) < 1 or len(name) > 64:
+            raise ValueError('Name must be between 1 and 64 characters long')
+        return name
+
+    @validates('author')
+    def validate_author(self, key, author):
+        if not author.isalpha():
+            raise ValueError('Author must contain only letters')
+        if len(author) < 1 or len(author) > 64:
+            raise ValueError('Author must be between 1 and 64 characters long')
+        return author
 
     def __repr__(self):
         return f"Book(ID: {self.id}, Name: {self.name}, Author: {self.author}, Year Published: {self.year_published}, Type: {self.book_type}, Status: {self.status})"
--- Python/Flask_Book_Library/project/customers/models.py
+++ Python/Flask_Book_Library/project/customers/models.py
@@ -1,9 +1,9 @@
 from project import db, app
+from sqlalchemy.orm import validates
 
 
 # Customer model
 class Customer(db.Model):
-    __tablename__ = 'customers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, index=True)
     city = db.Column(db.String(64))
@@ -16,6 +16,22 @@ class Customer(db.Model):
 
     def __repr__(self):
         return f"Customer(ID: {self.id}, Name: {self.name}, City: {self.city}, Age: {self.age})"
+
+    @validates("name")
+    def validate_name(self, key, name):
+        if not name.isalnum():
+            raise ValueError("Name must contain only alphanumeric characters")
+        if len(name) < 1 or len(name) > 64:
+            raise ValueError("Name must be between 1 and 64 characters long")
+        return name
+
+    @validates("city")
+    def validate_city(self, key, city):
+        if not city.isalpha():
+            raise ValueError("City must contain only letters")
+        if len(city) < 1 or len(city) > 64:
+            raise ValueError("City must be between 1 and 64 characters long")
+        return city
 
 
 with app.app_context():
